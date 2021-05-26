b358164bdfc9518f5e59cb2bad546a32b3481c5a
熊志成 <2430455821@qq.com>
2021-05-26 18:35:00 +0800

    添加对象池

---
+++ hcg/assets/subBundle/scripts/gameCommon/NodePool.ts
@@ -0,0 +1,36 @@
+/*
+ * @Author: Xiong ZhiCheng 
+ * @Date: 2021-05-25 10:20:55 
+ * @Description: 
+ */
+export class NodePool {
+
+    public static nodeMap: Map<cc.Prefab, [cc.Node]> = new Map();
+
+    public static instanceToPre: Map<cc.Node, cc.Prefab> = new Map();
+
+    public static getInstance(prefab: cc.Prefab): cc.Node {
+        let nodeList = this.nodeMap.get(prefab);
+        if (nodeList && nodeList.length) {
+            let targetNode = nodeList.pop();
+            targetNode.active = true;
+            return;
+        }
+
+        let node = cc.instantiate(prefab);
+        this.instanceToPre.set(node, prefab);
+        return node;
+    }
+
+    public static returnInstance(node: cc.Node): void {
+        let prefab = this.instanceToPre.get(node);
+        if (!prefab) {
+            node.destroy();
+            return;
+        }
+        node.active = false;
+        node.removeFromParent();
+        let nodeList = this.nodeMap.get(prefab);
+        nodeList.push(node);
+    }
+}
