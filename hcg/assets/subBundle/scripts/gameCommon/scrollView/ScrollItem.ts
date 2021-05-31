ac3e0af78ea0cd6bb825dcd40c93634a5b386f23
熊志成 <2430455821@qq.com>
2021-05-31 11:26:53 +0800

    删除多余代码

--- hcg/assets/subBundle/scripts/gameCommon/scrollView/ScrollItem.ts
+++ hcg/assets/subBundle/scripts/gameCommon/scrollView/ScrollItem.ts
@@ -8,9 +8,6 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export class ScrollItem extends cc.Component {
 
-    @property({ displayName: "文字", type: cc.Label })
-    private label: cc.Label = null;
-
     private data: any = null;
 
     public getData(): any {
